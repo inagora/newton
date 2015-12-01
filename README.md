bfe82190a6d3d1089cc16436dd7a597f55fc8492
poker <vrwonder@163.com>
2015-12-01 16:52:57 +0800

    add js compress

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # newton
+
+### todo list
+
+1. 增加文件编译缓存，加速编译
+3. 增加uglify之后的文件对比，是有压缩后变小才使用
+4. js compress 增加ignore目录或文件
+5. screw_ie8 http://javascript.sg/using-uglifys-screw-ie8-option-in-gulp/
+6. css prefix自动补全
+2. 增加图片base64
--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  * Newton build on nodejs 5.1.0
  */
 const process = require('process');
-const fs = require('fs');
 const cp = require('child_process');
 const path = require('path');
 const util = require('./lib/util');
@@ -22,51 +21,66 @@ util.mkdir(gOutput, true);
 
 var conf;
 try{
-	conf = require(gRoot+'/config.json');
+	conf = require(gRoot+'/newton.config.json');
 }catch(e){
+	gLog.error(JSON.stringify(e));
+	return;
 	conf = {};
 }
-
 global.gConf = util.extend(require('./lib/default-config.json'), conf);
-if(!Array.isArray(gConf.dirs))
-	gConf.dirs = [gConf.dirs];
+if(!Array.isArray(gConf.app))
+	gConf.app = [gConf.app];
 
 
 global.gCompileRoot = path.join('/tmp/newton/'+gConf.project+'-'+util.now('-'));
 util.mkdir(gCompileRoot, true);
 global.gLog = require('./lib/log')(gCompileRoot);
+
 /**
  * 拷贝文件到/tmp/newton/{project}下，准备编译
  */
-
-gConf.dirs.forEach(function(dir){
-	if(dir.tpl){
-		util.mkdir(dir.tpl, gCompileRoot, true);
-		cp.execSync('cp -R '+path.join(gRoot, dir.tpl,'/')+'* '+path.join(gCompileRoot, dir.tpl, '/'))
+if(gConf.app.length<=0){
+	gLog.warning('因为没有配置app,直接通出');
+	return;
+}
+gConf.app.forEach(function(appConf){
+	appConf = util.extend(appConf, gConf, false);
+	if(appConf.tpl){
+		util.mkdir(appConf.tpl, gCompileRoot, true);
+		cp.execSync('cp -R '+path.join(gRoot, appConf.tpl,'/')+'* '+path.join(gCompileRoot, appConf.tpl, '/'))
 	}
-	if(dir.res){
-		util.mkdir(dir.res, gCompileRoot, true);
-		cp.execSync('cp -R '+path.join(gRoot, dir.res,'/')+'* '+path.join(gCompileRoot, dir.res, '/'))
+	if(appConf.res){
+		util.mkdir(appConf.res, gCompileRoot, true);
+		cp.execSync('cp -R '+path.join(gRoot, appConf.res,'/')+'* '+path.join(gCompileRoot, appConf.res, '/'))
 	}
 });
 
-var jobs = ['webpack', 'jsCompress', 'cssCompress', 'staticToCdn'];
+/**
+ * 执行各个编译任务
+ */
+var jobs = [
+	//'webpack',
+//'jsCompress', 
+'cssCompress']//, 'staticToCdn'];
+var jobName,
+	startTime;
 function executeJob(){
 	if(jobs.length>0){
-		var jobName = jobs.shift();
+		jobName = jobs.shift();
 		gLog('Newton: 开始执行任务：'+jobName);
+		startTime = new Date();
 		require('./lib/job/'+jobName).run().then(function(data){
-			gLog('Newton: 任务执行完成：'+jobName);
+			gLog('Newton: 任务执行完成：'+jobName+', 共耗时：'+util.timeFormat(new Date - startTime));
 			executeJob();
 		}, function(data){
-			gLog('Newton: 任务执行失败：'+jobName);
+			gLog.error('Newton: 任务执行失败：'+jobName);
 			if(data){
 				data = typeof data=='string' ? data: JSON.stringify(data);
 				gLog.error(data);
 			}
 		}).catch(function(e){
-			gLog('Newton: 任务执行出现意外：'+jobName);
-			gLog.error(e.getMessage());
+			gLog.error('Newton: 任务执行出现意外：'+jobName);
+			console.log(e, e.line);
 		});
 	}else{
 		gLog('NT编译完成');
